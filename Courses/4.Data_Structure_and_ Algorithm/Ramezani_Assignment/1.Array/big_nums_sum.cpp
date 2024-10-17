a7693c15140ee4636c1d62bd55c34fe5fab3bce1
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2024-10-17 21:51:01 +0330

    Solve ramezani assignment

--- Courses/4.Data_Structure_and_ Algorithm/Ramezani_Assignment/1.Array/big_nums_sum.cpp
+++ Courses/4.Data_Structure_and_ Algorithm/Ramezani_Assignment/1.Array/big_nums_sum.cpp
@@ -8,9 +8,10 @@ int main(){
     int b_idx = b.length()-1;
 
     string tmp_zeros = "";
-    int idx;
     for(int i=0; i<abs(a_idx-b_idx); i++)
         tmp_zeros += "0";
+
+    int idx;
     if(a_idx > b_idx){
         idx = a_idx;
         b = tmp_zeros + b;
@@ -22,7 +23,7 @@ int main(){
     string reverse_sum = "";
     int carry = 0;
     while(idx >= 0){
-        int tmp_sum = a[idx]-'0' + b[idx]-'0' + carry;
+        int tmp_sum = (a[idx]-'0') + (b[idx]-'0') + carry;
 
         char digit = (tmp_sum)%10 + '0';
         reverse_sum += digit;
@@ -33,7 +34,7 @@ int main(){
     if(carry)
         reverse_sum += "1";
     
-    for(int i=reverse_sum.length(); i>=0; i--)
+    for(int i=reverse_sum.length()-1; i>=0; i--)
         cout << reverse_sum[i];
     
 
