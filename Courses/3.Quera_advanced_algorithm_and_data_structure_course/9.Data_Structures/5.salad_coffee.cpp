0fd230d6877b70faa501df57341eebd195cc4438
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2024-10-31 23:09:35 +0330

    Work on quera codes

--- Courses/3.Quera_advanced_algorithm_and_data_structure_course/9.Data_Structures/5.salad_coffee.cpp
+++ Courses/3.Quera_advanced_algorithm_and_data_structure_course/9.Data_Structures/5.salad_coffee.cpp
@@ -8,28 +8,33 @@ class Min_Heap{
 public:
     pair<string, int>* customers[MAX_SIZE]; //<name, expire date>
     int last_ind;
-    bool (*compare) (pair<string, int>*, pair<string, int>*);
+    int (*compare) (pair<string, int>*, pair<string, int>*);
 
-    Min_Heap(bool (*funcPtr)(pair<string, int>*, pair<string, int>*)){
+    Min_Heap(int (*funcPtr)(pair<string, int>*, pair<string, int>*)){
         last_ind = 0;
         customers[0] = new pair<string, int> {"", 0};
         compare = funcPtr;
     }
 
-    static bool compare_by_expire_date(pair<string, int>* a, pair<string, int>* b){
-        return a->second > b->second;
+    static int compare_by_expire_date(pair<string, int>* a, pair<string, int>* b){
+        if(a->second > b->second)
+            return 1;
+        if(a->second < b->second)
+            return -1;
+        return 0;
     }
-    static bool compare_by_name(pair<string, int>* a, pair<string, int>* b){
-        return (a->first.compare(b->first) > 0) ? 1:0;
+    static int compare_by_name(pair<string, int>* a, pair<string, int>* b){
+        return a->first.compare(b->first);
     }
 
+    int get_size(){ return last_ind;}
     int get_father(int i){ return i/2; }
-    int get_left_child(int i){ return 2*i + 1; }
+    int get_left_child(int i){ return 2*i; }
     int get_right_child(int i){ return 2*i+1; }
 
     void bubble_up(int i){
         int father_ind = get_father(i);
-        while(father_ind >= 0 && compare(father_ind, i)){
+        while(father_ind > 0 && compare(customers[father_ind], customers[i]) > 0){
             pair<string, int>* tmp = customers[i];
             customers[i] = customers[father_ind];
             customers[father_ind] = tmp;
@@ -41,40 +46,82 @@ public:
 
     void bubble_down(int i){
         while(i <= this->last_ind){
-            int child_ind = -1;
+            int child_ind = i;
 
             int left_child_ind = get_left_child(i);
-            if(left_child_ind <= this->last_ind && compare(customers[i], customers[left_child_ind]))
+            if(left_child_ind <= this->last_ind && compare(customers[i], customers[left_child_ind]) > 0)
                 child_ind = left_child_ind;
 
             int right_child_ind = get_right_child(i);
-            if(right_child_ind <= this->last_ind && compare(customers[i], customers[right_child_ind]))
+            if(right_child_ind <= this->last_ind && compare(customers[child_ind], customers[right_child_ind]) > 0)
                 child_ind = right_child_ind;
 
-            if(child_ind == -1)
+            if(child_ind == i)
                 break;
 
             pair<string, int>* tmp = customers[i];
             customers[i] = customers[child_ind];
             customers[child_ind] = tmp;
+            i = child_ind;
         }
     }
 
-    void insert(pair<string, int>* newCust){
+    void insert(string name, int expiry){
+        pair<string, int>* newCust = new pair<string, int>{name, expiry};
         this->customers[++last_ind] = newCust;
         bubble_up(last_ind);
     }
 
+    int get_min_expiry(){
+        return (last_ind == 0) ? 0:customers[1]->second;
+    }
+
     pair<string, int> pop_min(){
         if(last_ind == 0)
-            return;
+            return *customers[0];
 
-        pair<string, int> min = *customers[0];
-        customers[0] = customers[last_ind];
+        pair<string, int> min = *customers[1];
+        delete customers[1];
+        customers[1] = customers[last_ind];
+        customers[last_ind] = nullptr;
         last_ind--;
-        bubble_down(0);
+        bubble_down(1);
 
         return min;
+    }
+
+    int find_customer(pair<string, int> cust, int curr_ind = 1){
+        if(curr_ind > last_ind)
+            return -1;
+        if(customers[curr_ind]->first.compare(cust.first) == 0)
+            return curr_ind;
+
+        int left_result = find_customer(cust, get_left_child(curr_ind));
+        if(left_result != -1)
+            return left_result;
+
+        return find_customer(cust, get_right_child(curr_ind));
+    }
+
+    void delete_customer(pair<string, int> cust){
+        int cust_ind = find_customer(cust);
+        if(cust_ind == -1)
+            return;
+
+        delete customers[cust_ind];
+        customers[cust_ind] = customers[last_ind];
+        customers[last_ind] = nullptr;
+        last_ind--;
+        bubble_down(cust_ind);
+        
+    }  
+
+    void clear(){
+        while(last_ind != 0){
+            delete customers[last_ind];
+            customers[last_ind] = nullptr;
+            last_ind--;
+        }
     }
 };
 
@@ -82,6 +129,7 @@ public:
 int main(){
     Min_Heap* heap_by_expiry = new Min_Heap(Min_Heap::compare_by_expire_date);
     Min_Heap* heap_by_name = new Min_Heap(Min_Heap::compare_by_name);
+    Min_Heap* deleteds = new Min_Heap(Min_Heap::compare_by_name);
 
     int q; cin >> q;
     cin.ignore();
@@ -92,11 +140,38 @@ int main(){
         int k; ss >> k;
         for(int p=0; p<k; p++){
             string name; int expiry_days;
-            pair<string, int>* newCust = new pair<string, int>{name, day + expiry_days - 1};
-            heap_by_expiry->insert(newCust);
-            heap_by_name->insert(newCust);
+            ss >> name; ss >> expiry_days;
+            // pair<string, int>* newCust = new pair<string, int>{name, day + expiry_days - 1};
+            heap_by_expiry->insert(name, day + expiry_days - 1);
+            heap_by_name->insert(name, day + expiry_days - 1);
         }
-        //TODO: handle other methods
+
+        pair<string, int> deleted_cust;
+        if(day != q){
+            //delete finished accounts
+            while(heap_by_expiry->get_min_expiry() == day){
+                deleted_cust = heap_by_expiry->pop_min();
+                heap_by_name->delete_customer(deleted_cust);
+                deleteds->insert(deleted_cust.first, deleted_cust.second);
+            }
+
+            //fun delete
+            deleted_cust = heap_by_name->pop_min();
+            heap_by_expiry->delete_customer(deleted_cust);
+            deleteds->insert(deleted_cust.first, deleted_cust.second);
+        }else{
+            //delete all
+            while(heap_by_name->get_size() != 0){
+                deleted_cust = heap_by_name->pop_min();
+                deleteds->insert(deleted_cust.first, deleted_cust.second);
+            }
+            heap_by_expiry->clear();
+        }
+
+        while(deleteds->get_size() != 0){
+            cout << deleteds->pop_min().first << " ";
+        }
+        cout << endl;
     }
 
 
