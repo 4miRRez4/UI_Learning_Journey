bbf731ee567f4baebc5014d0029e89df4089784b
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2024-11-06 20:54:25 +0330

    solve

---
+++ Courses/4.Data_Structure_and_ Algorithm/Ramezani_Assignment/4.Stack_Queue/win_game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main(){
+    int n; int k; cin >> n >> k;
+    for(int i=0; i<k; i++){
+        int tmp; cin >> tmp;
+        q1.push(tmp);
+    }
+    q2 = q1;
+
+    for(int i=0; i<n; i++){
+
+    }
+
+
+    return 0;
+}
