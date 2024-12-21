968bef517c5c6f18257015b5b9dd8f54c57f8ca9
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2024-12-21 11:45:58 +0330

    Add pointer and str assignments

---
+++ Other_Codes/3.Teaching_Assistant_Material/1.Fundamental_of_computer_programming/encoder/encoder.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX_SIZE = 1000; 
+int main()
+{
+    char message[MAX_SIZE] ={};
+
+    cin.getline(message, MAX_SIZE);
+    int message_len = strlen(message);
+
+	int numberOfElements = message_len / 4;
+	if(message_len % 4 != 0) numberOfElements++;
+
+    int encriptedArr[numberOfElements] = {};
+    int arrInd = -1;
+    for(int c=0; c<message_len; c++)
+    {
+        int asciiCode = (int)message[c];
+        if(c%4 == 0) 
+            arrInd++;
+        encriptedArr[arrInd] = encriptedArr[arrInd] | (asciiCode << 8*(c%4));
+    }
+
+    cout << numberOfElements << endl;
+    for(int i=0; i<numberOfElements; i++)
+        cout << encriptedArr[i] << endl;
+
+
+    return 0;
+}
---
+++ Other_Codes/3.Teaching_Assistant_Material/1.Fundamental_of_computer_programming/maze/test_case_generator.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+
+void print_maze(int* maze, int n, string& answer)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            answer += to_string(*(maze + i * n + j)) + " ";
+            if(*(maze + i * n + j) < 10)
+                answer += " ";
+        }
+        answer += "\n";
+    }
+}
+
+
+void generateSpiralMaze(int* maze, int n, int origin, int destination, 
+                        int& originRow, int& originCol, int& destRow, int& destCol)
+{
+    int num = 0;
+    int bottom = n - 1, right = n - 1, up = 0, left = 0;
+
+    while(num < n * n)
+    {
+        // Filling bottom side
+        for(int c = left; c <= right; c++)
+        {
+            *(maze + bottom * n + c) = ++num;
+            if (num == origin) originRow = bottom, originCol = c;
+            else if (num == destination) destRow = bottom, destCol = c;
+        }
+        bottom--;
+
+        // Filling right side
+        for(int r = bottom; r >= up; r--)
+        {
+            *(maze + r * n + right) = ++num;
+            if (num == origin) originRow = r, originCol = right;
+            else if (num == destination) destRow = r, destCol = right;
+        }
+        right--;
+
+        // Filling up side
+        for(int c = right; c >= left; c--)
+        {
+            *(maze + up * n + c) = ++num;
+            if (num == origin) originRow = up, originCol = c;
+            else if (num == destination) destRow = up, destCol = c;
+        }
+        up++;
+
+        // Filling left side
+        for(int r = up; r <= bottom; r++)
+        {
+            *(maze + r * n + left) = ++num;
+            if (num == origin) originRow = r, originCol = left;
+            else if (num == destination) destRow = r, destCol = left;
+        }
+        left++;
+    }
+}
+
+string mazeQ(int n, int origin, int destination)
+{
+    int originRow = 0, originCol = 0, destRow = 0, destCol = 0;
+
+    int* maze = (int*)calloc(n * n, sizeof(int));
+
+    generateSpiralMaze(maze, n, origin, destination, originRow, originCol, destRow, destCol);
+
+    int horizontal = destCol - originCol;
+    int vertical = destRow - originRow;
+
+    string answer = "";
+    answer += to_string(abs(horizontal)) + " " + ((horizontal > 0) ? "R" : "L") + "\n";
+    answer += to_string(abs(vertical)) + " " + ((vertical < 0) ? "U" : "D") + "\n";
+
+    print_maze(maze, n, answer);
+
+    free(maze);
+    return answer;
+}
+
+void generateTestCase(string inFileName, string outFileName) {
+    FILE* inFile = fopen(inFileName.c_str(), "w");
+    if (!inFile) {
+        cerr << "Error: Could not open file " << inFileName << " for writing." << endl;
+        return;
+    }
+
+    int minVal1 = 2, maxVal1 = 9;
+    int randNum1 = minVal1 + (rand() % (maxVal1 - minVal1 + 1));
+
+    int minVal2 = 1, maxVal2 = randNum1 * randNum1;
+    int randNum2 = minVal2 + (rand() % (maxVal2 - minVal2 + 1));
+    int randNum3;
+    do {
+        randNum3 = minVal2 + (rand() % (maxVal2 - minVal2 + 1));
+    } while (randNum3 == randNum2);
+
+    fprintf(inFile, "%d %d %d\n", randNum1, randNum2, randNum3);
+    fclose(inFile);
+
+    FILE* outFile = fopen(outFileName.c_str(), "w");
+    if (!outFile) {
+        cerr << "Error: Could not open file " << outFileName << " for writing." << endl;
+        return;
+    }
+
+    string answer = mazeQ(randNum1, randNum2, randNum3);
+    fprintf(outFile, "%s", answer.c_str());
+
+
+    fclose(outFile);
+}
+
+int main() {
+    srand(time(nullptr));
+
+    int n; cin >> n;
+    for(int i=1; i<=n; i++){
+        string inFileName = "input" + to_string(i) + ".txt";
+        string outFileName = "output" + to_string(i) + ".txt";
+        generateTestCase(inFileName, outFileName);
+    }
+
+    return 0;
+}
