fcd10d8c8d820bcfbf30f71b1c4259bde89165b8
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2024-12-24 11:45:43 +0330

    Solve quera assignment

--- Courses/3.Quera_advanced_algorithm_and_data_structure_course/6.Dynamic_Programming/4.max_sum_rectangle.cpp
+++ Courses/3.Quera_advanced_algorithm_and_data_structure_course/6.Dynamic_Programming/4.max_sum_rectangle.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 typedef long long ll;
 
+const int MAX_SIZE = 500 + 10;
+const ll INF = 1e18;
+ll dp[MAX_SIZE];
+
 int main(){
     int rows, cols; cin >> rows >> cols;
     
-    int nums[rows][cols];
+    int nums[MAX_SIZE][MAX_SIZE];
     for(int r=0; r<rows; r++)
         for(int c=0; c<cols; c++)
             cin >> nums[r][c];
 
-    ll max_rec_sum = -1e18;
+    ll max_rec_sum = -INF;
     for(int left_c=0; left_c<cols; left_c++){
         ll row_sum[rows] = {};
         for(int right_c=left_c; right_c<cols; right_c++){
@@ -19,14 +23,11 @@ int main(){
             for(int row=0; row<rows; row++)
                 row_sum[row] += nums[row][right_c];
             
-            ll curr_subset_sum=-1e18;
-            ll max_row_sum=-1e18;
+            dp[0] = 0;
             for(int i=0; i<rows; i++){
-                curr_subset_sum = max(row_sum[i], row_sum[i] + curr_subset_sum);
-                max_row_sum = max(max_row_sum, curr_subset_sum);
+                dp[i+1] = max(row_sum[i], row_sum[i] + dp[i]);
+                max_rec_sum = max(max_rec_sum, dp[i+1]);
             }
-            
-            max_rec_sum = max(max_rec_sum, max_row_sum);
         }
     }
 
