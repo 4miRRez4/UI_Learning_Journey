a522f291878391cc94da475589ea1a4f4242d396
Karimi <mokarimi22.4@gmail.com>
2025-01-28 00:23:48 +0330

    Add basic graph property methods. Implements core graph analysis methods: - numVertices() - returns total number of users - vertices() - returns list of all user IDs - numEdges() - calculates total connections - edges() - returns all connections as pairs

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,11 +1,44 @@
 #include "Graph.h"
 
-Graph::Graph()
+int Graph::numVertices()
 {
-
+    return users.size();
+    ;
 }
 
-Graph::~Graph()
+vector<string> Graph::vertices()
 {
+    vector<string> vList;
+    for (auto &id : users)
+    {
+        vList.push_back(id.first);
+    }
+    return vList;
+}
 
+int Graph::numEdges()
+{
+    int count = 0;
+    for (auto &edge : adjacencyList)
+    {
+        count += edge.second.size();
+    }
+    return count / 2;
+}
+
+vector<pair<string, string>> Graph::edges()
+{
+    vector<pair<string, string>> eList;
+    for (auto &edge : adjacencyList)
+    {
+        string u = edge.first;
+        for (auto &v : edge.second)
+        {
+            if (u < v)
+            {
+                eList.push_back({u, v});
+            }
+        }
+    }
+    return eList;
 }
