cb44d009e6be36df145d47a0aa8341eae499953a
Karimi <mokarimi22.4@gmail.com>
2025-01-28 21:18:03 +0330

    add calculateSkillSimilarity method. Used set intersection to find common skills

--- src/RecommendationManager.cpp
+++ src/RecommendationManager.cpp
@@ -17,6 +17,31 @@ int RecommendationManager::calculateWorkplaceSimilarity(User &user1, User &user2
     return 0;
 }
 
+int RecommendationManager::calculateSkillSimilarity(User &user1, User &user2)
+{
+    vector<string> user1_skill = user1.getSpecialties();
+    vector<string> user2_skill = user2.getSpecialties();
+    vector<string> common_skils;
+    int i = 0, j = 0;
+    while (i < user1_skill.size() && j < user2_skill.size())
+    {
+        if (user1_skill[i] == user2_skill[j])
+        {
+            common_skils.push_back(user1_skill[i]);
+            i++;
+            j++;
+        }
+        else if (user1_skill[i] < user2_skill[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    return common_skils.size() * SKILL_MATCH_SCORE;
+}
 
 vector<string> RecommendationManager::bfsShortestPath(string &start, string &target)
 {
