e2c324766754393834109a75cc176a788dec2b34
Karimi <mokarimi22.4@gmail.com>
2025-01-29 16:57:20 +0330

    New class to hold weights configuration

--- include/RecommendationManager.h
+++ include/RecommendationManager.h
@@ -12,13 +12,30 @@
 class RecommendationManager
 {
 public:
+    class WeightsConfig
+    {
+    public:
+        // default weight
+        int degreeWeight = 40;
+        int workplaceWeight = 35;
+        int skillWeight = 25;
+
+        WeightsConfig() = default;
+        WeightsConfig(int degreeW, int workplaceW, int skillW)
+        {
+            if (degreeW + workplaceW + skillW != 100)
+            {
+                throw std::invalid_argument("Invalid weights: sum of weights must be 100");
+            }
+            degreeWeight = degreeW;
+            workplaceWeight = workplaceW;
+            skillWeight = skillW;
+        }
+    };
     RecommendationManager(Graph &graph);
     vector<string> getRecommandations(string &userId, int numRecommendations = 20);
 
 private:
-    static const int DEGREE_WEIGHT = 40;
-    static const int WORKPLACE_WEIGHT = 35;
-    static const int SKILL_WEIGHT = 25;
     static const int MAX_DEGREE_SCORE = 5;
     static const int WORKPLACE_MATCH_SCORE = 10;
     static const int SKILL_MATCH_SCORE = 5;
