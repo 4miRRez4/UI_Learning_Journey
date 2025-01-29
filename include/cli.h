d5ddefdea3900c72933202e43f8749d1a9c90940
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-01-29 16:53:29 +0330

    feat(cli): Add command line interface

---
+++ include/cli.h
@@ -0,0 +1,16 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include "dataBase.h"
+
+void displayMenu();
+void handleUserInput(Database& db);
+void createTable(Database& db);
+void insertRecord(Database& db);
+void updateRecord(Database& db);
+void deleteRecord(Database& db);
+void searchRecord(Database& db);
+void printAllRecords(Database& db);
+void performAggregation(Database& db);
+
+#endif
