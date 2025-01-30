e0300dbc00f2035e53fd011bff0509964646c678
Karimi <mokarimi22.4@gmail.com>
2025-01-30 19:12:25 +0330

    add main to start

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 #include "../include/dataBase.h"
 #include "../include/cli.h"
+#include "../include/Interface.h"
+#include "../include/JSONReader.h"
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main() {
-    Database* db = new Database();
+int main()
+{
+    Database *db = new Database();
     handleUserInput(db);
 
+    Graph graph;
+    JSONReader JReader("../data/users.json");
+    vector<User> users = JReader.readUsers();
+    for (User &user : users)
+    {
+        graph.insertVertex(user);
+        for (const string &connectionId : user.getConnections())
+        {
+            graph.insertEdge(user.getId(), connectionId);
+        }
+    }
+    RecommendationManager recManager(graph);
+    Interface interface(graph, recManager);
+    interface.start();
     return 0;
 }
