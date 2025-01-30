20bbb90ece8c8c7a29d824796107968c49e25c68
Karimi <mokarimi22.4@gmail.com>
2025-01-30 12:48:45 +0330

    Add header file and  constructor

--- include/Interface.h
+++ include/Interface.h
@@ -1,10 +1,24 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include "graph.h"
+#include "JSONReader.h"
+#include "RecommendationManager.h"
+#include "CommunicationQuality.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
 class Interface
 {
 public:
+    Interface(Graph &g, RecommendationManager &rM)
+        : graph(g), recManager(rM), currentUser("") {}
+
 private:
+    Graph &graph;
+    RecommendationManager &recManager;
+    string currentUser;
 };
 
 #endif
