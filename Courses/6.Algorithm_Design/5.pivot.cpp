f25bca989f933bc01b196be7f7b620d6ba47638a
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-02-27 16:45:25 +0330

    Solve quera assignments

---
+++ Courses/6.Algorithm_Design/5.pivot.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int l, int r){
+    int pivot_val = arr[l];
+
+    int ls = l;
+    for(int i=l+1; i<=r; i++){
+        if(arr[i] <= pivot_val){
+            swap(arr[i], arr[++ls]);
+        }
+    }
+    swap(arr[ls], arr[l]);
+    
+    return ls;
+}
+
+void quick_sort(int arr[], int l, int r){
+    if(r <= l)
+        return;
+    
+    int p = partition(arr, l, r);
+    cout << arr[p] << " ";
+
+    quick_sort(arr, l, p-1);
+    quick_sort(arr, p+1, r);
+}
+
+int main(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+
+    quick_sort(arr, 0, n-1);
+    
+    cout << endl;
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
