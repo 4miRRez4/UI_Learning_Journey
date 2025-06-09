ebe08e079ffdb3b85546a429a99b79504e83cefa
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-06-09 17:09:33 +0330

    Organize

---
+++ 1.Basic_Programming/0.Computer_Skills_WorkShop/Git_Assignment/myProject.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void mirrorFunc()
+{
+	cout << "I am the best" << endl;
+}
+void Voice()
+{
+	cout << "You hear my voice from new_branch" << endl;
+}
+
+int main()
+{
+	mirrorFunc();
+	Voice();
+	return 0;
+}
