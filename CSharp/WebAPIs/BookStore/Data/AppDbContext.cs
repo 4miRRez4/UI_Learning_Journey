49750e9809d14cadb1db5016f66603d292e7f95a
Amirreza Mohammadi Yeganeh <a.mohammadi@depnaco.ir>
2025-07-01 15:49:17 +0330

    feat: seed data in AppDbContext using ai generated data

--- CSharp/WebAPIs/BookStore/Data/AppDbContext.cs
+++ CSharp/WebAPIs/BookStore/Data/AppDbContext.cs
@@ -13,7 +13,12 @@ namespace BookStore.Data
         public DbSet<Author> Authors { get; set; }
         public DbSet<Review> Reviews { get; set; }
 
-
+        //Seed Authors
+        ModelBuilder.Entity<Author>.HasData(
+            new Author { Id = 1, Name = "Abbas Maroufi", BirthDate = new DateTime(1940, 1, 1)}
+            new Author { Id = 2, Name = "George R.R. Martin", BirthDate = new DateTime(1950, 2, 2) 
+            new Author { Id = 1, Name = "J.K. Rowling", BirthDate = new DateTime(1960, 3, 3) }
+        )
 
     }
 }
