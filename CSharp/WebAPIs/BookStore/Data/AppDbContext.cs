3d555304db2ea2955753cecab8b98b1c4506c5f9
Amirreza Mohammadi Yeganeh <a.mohammadi@depnaco.ir>
2025-07-01 15:40:05 +0330

    feat: add AppDbContext with Book, Author, User, and Review Dbsets

---
+++ CSharp/WebAPIs/BookStore/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
+
+    }
+}
