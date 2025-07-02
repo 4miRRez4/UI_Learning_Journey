fb73b17075c46f0a4fc02ff64069ef37265c9192
Amirreza Mohammadi Yeganeh <a.mohammadi@depnaco.ir>
2025-07-02 18:09:44 +0330

    feat: configure program.cs

--- CSharp/WebAPIs/BookStore/Program.cs
+++ CSharp/WebAPIs/BookStore/Program.cs
@@ -1,8 +1,28 @@
+using BookStore.Data;
+using BookStore.Services;
+using BookStore.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "BookStore API", Version = "v1" });
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBookService, BookService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -10,9 +30,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
