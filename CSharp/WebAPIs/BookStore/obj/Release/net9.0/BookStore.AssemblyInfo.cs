2e3e0a6b573cfb17f4c34e4c25afc91911a7c70a
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-07-15 17:13:11 +0330

    build: add required packaged for authentication (Identity, JWT)

--- CSharp/WebAPIs/BookStore/obj/Release/net9.0/BookStore.AssemblyInfo.cs
+++ CSharp/WebAPIs/BookStore/obj/Release/net9.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9192b12f5f9755ec05ad660b18f82e2e2ef4979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14816b2060329751909e954b53d9cfef5ba7e3f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
