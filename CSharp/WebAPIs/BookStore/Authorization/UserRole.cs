172c707d22f65ad46295ae8bc8fe6db3a9a10cb5
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-07-16 11:47:23 +0330

    feat: add static properties for user roles

---
+++ CSharp/WebAPIs/BookStore/Authorization/UserRole.cs
@@ -0,0 +1,8 @@
+namespace BookStore.Authorization;
+
+public static class UserRoles
+{
+    public const string Admin = "Admin";
+    public const string Seller = "Seller";
+    public const string Customer = "Customer";
+}
