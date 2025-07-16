8a94e41dd20033150e425c67332738df8e535e19
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-07-16 10:49:54 +0330

    fix: remove duplicated properties

--- CSharp/WebAPIs/BookStore/Models/ApplicationUser.cs
+++ CSharp/WebAPIs/BookStore/Models/ApplicationUser.cs
@@ -7,8 +7,6 @@ namespace BookStore.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string? UserName { get; set; }
-        public string Email { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         //navigation properties
---
+++ CSharp/WebAPIs/BookStore/Models/Auth/RolePermission.cs
@@ -0,0 +1,10 @@
+
+
+namespace BookStore.Models.Auth
+{
+    public class RolePermission
+    {
+        public int RoleId { get; set; }
+        public int PermissionId { get; set; }
+    }
+}
