b8ff5cfd70b6e33e44acc31c4e2d0e21d0365610
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-07-27 12:39:42 +0330

    fix(test): rebuild test project for proper version matching

--- CSharp/WebAPIs/BookStore.Tests/obj/Debug/net9.0/BookStore.Tests.AssemblyInfo.cs
+++ CSharp/WebAPIs/BookStore.Tests/obj/Debug/net9.0/BookStore.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad33275382613cc7051580d577cedd48e96890e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ee703e5596fc9ae2b3070392baff13d1105bd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
