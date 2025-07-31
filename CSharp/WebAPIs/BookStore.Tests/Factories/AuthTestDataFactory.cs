60e09b2f74f1467741ca9617f6e5ae228f4a972a
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-07-31 17:00:21 +0330

    test(auth): add AuthTestDataFactory for tests data generation

---
+++ CSharp/WebAPIs/BookStore.Tests/Factories/AuthTestDataFactory.cs
@@ -0,0 +1,32 @@
+﻿
+using BookStore.Dtos.Auth;
+using BookStore.Models;
+
+namespace BookStore.Tests.Factories
+{
+    public static class AuthTestDataFactory
+    {
+        public static RegisterDto CreateValidRegisterDto() => new RegisterDto
+        {
+            UserName = "TestUser",
+            Email = "TestUser@test.com",
+            Password = "TestPassword123",
+            ConfirmPassword = "TestPassword123"
+        };
+
+        public static LoginDto CreateValidLoginDto() => new LoginDto
+        {
+            Email = "test@example.com",
+            Password = "ValidPassword123!"
+        };
+
+        public static ApplicationUser CreateTestUser(string email = "TestUser@test.com", string userName = "TestUser")
+        => new ApplicationUser
+        {
+            Id = Guid.NewGuid().ToString(),
+            Email = email,
+            UserName = userName
+        };
+
+    }
+}
