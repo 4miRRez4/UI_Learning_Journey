3b16b070893031de01ec1bad3a2df47141f7cde8
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-07-31 17:09:38 +0330

    test(auth): implement AuthResultBuilder for complext assersions for returns of AuthService methods

---
+++ CSharp/WebAPIs/BookStore.Tests/Builders/AuthResultBuilder.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Dtos.Auth;
+
+namespace BookStore.Tests.Builders
+{
+    class AuthResultBuilder
+    {
+        private AuthResult _authResult = new AuthResult();
+
+        public AuthResultBuilder WithSuccess(bool success)
+        {
+            _authResult.Success = success;
+            return this;
+        }
+
+        public AuthResultBuilder withUserId(string userId)
+        {
+            _authResult.UserId = userId;
+            return this;
+        }
+
+        public AuthResultBuilder WithError(string error)
+        {
+            _authResult.Errors = new[] { error };
+            return this;
+        }
+
+        public AuthResult Build() => _authResult;
+    }
+}
