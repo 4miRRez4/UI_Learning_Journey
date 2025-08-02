56691a8ddc29644c64a80b7d1ce0502f66138435
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-08-02 16:33:47 +0330

    test(integration): add CustomWebApplicationFactory and ControllerTestBase

---
+++ CSharp/WebAPIs/BookStore.Tests/Integration/ControllerTestBase.cs
@@ -0,0 +1,37 @@
+﻿
+using BookStore.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
+using Xunit.Abstractions;
+
+namespace BookStore.Tests.Integration
+{
+    public abstract class ControllerTestBase : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        protected readonly HttpClient Client;
+        protected readonly Mock<IAuthService> AuthServiceMock;
+        protected readonly ITestOutputHelper Output;
+
+        protected ControllerTestBase(
+            CustomWebApplicationFactory<Program> factory,
+            ITestOutputHelper output)
+        {
+            Client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false,
+                HandleCookies = true
+            });
+            AuthServiceMock = factory.AuthServiceMock;
+            Output = output;
+
+            AuthServiceMock.Invocations.Clear();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+    }
+}
---
+++ CSharp/WebAPIs/BookStore.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    public Mock<IAuthService> AuthServiceMock { get; } = new();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.AddSingleton(AuthServiceMock.Object);
+        });
+
+        builder.UseContentRoot(Directory.GetCurrentDirectory());
+    }
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        var testHost = base.CreateHost(builder);
+
+        builder.ConfigureWebHost(webBuilder =>
+        {
+            webBuilder.UseTestServer();
+        });
+
+        return testHost;
+    }
+}
