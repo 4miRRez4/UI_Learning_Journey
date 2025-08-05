7195263f538da55cbe4a2ee5308b1d05ad50fa9b
4miRRez4 <amirreza.mohamadi.yegane@gmail.com>
2025-08-05 18:51:04 +0330

    feat(TestWebApplicationFactory): add application factory which handles test dbcontext for usage in e2e tests

---
+++ CSharp/WebAPIs/BookStore.Tests/E2E/TestWebApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BookStore.Data;
+using Testcontainers.MsSql;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace BookStore.Tests.E2E
+{
+    public class TestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2019-latest")
+            .WithPassword("Strong(!)Password")
+            .Build();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                if (descriptor != null) services.Remove(descriptor);
+
+                services.AddDbContext<TestAppDbContext>(options =>
+                {
+                    options.UseSqlServer(_dbContainer.GetConnectionString(),
+                        sqlOpt => sqlOpt.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
+                });
+
+                services.AddScoped<AppDbContext>(sp => sp.GetRequiredService<TestAppDbContext>());
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<TestAppDbContext>();
+                db.Database.EnsureCreated();
+                db.Database.Migrate();
+            });
+        }
+
+        public async Task InitializeAsync() => await _dbContainer.StartAsync();
+        public new async Task DisposeAsync() => await _dbContainer.DisposeAsync();
+    }
+}
